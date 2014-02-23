0acc5de2e18bff2f6b5c8a382af91862ea6a61a9
boluge <boluge@gmail.com>
2014-02-23 19:34:05 +0100

    add ie 7

--- header.php
+++ header.php
@@ -13,10 +13,8 @@
 
 		<!--[if lt IE 9]>
 			<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
-			<script src="//s3.amazonaws.com/nwapi/nwmatcher/nwmatcher-1.2.5-min.js"></script>
 			<script src="//html5base.googlecode.com/svn-history/r38/trunk/js/selectivizr-1.0.3b.js"></script>
 			<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
-			<script src="<?php echo get_template_directory_uri(); ?>/js/rem.min.js"></script>
 			<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/grid_ie.css">
 		<![endif]-->
 
